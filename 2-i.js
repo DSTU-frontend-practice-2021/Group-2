c41112a11f62749712d6ab73969325c684fa8a8b
Matsko Egor <76002087+fuett966@users.noreply.github.com>
2021-06-27 00:28:29 +0300

    fixes
    fix: simpler solution ex js
    fix: the circle may vary depending on the resolution of the window

--- 2-i.js
+++ 2-i.js
@@ -1,18 +1,5 @@
-function highAndLow(numbers){
-    var nums = numbers.split(" ").map(parseFloat)
-      .filter(function(v){ return v===v });
-    var max = nums[0];
-    var min = nums[nums.length-1];
-    for (let i = 0; i < nums.length; i++) 
-    { 
-      if (nums[i]>max){
-        max = nums[i];
-      }
-      if (nums[i]<min){
-        min = nums[i];
-      }
-    } 
-    return max + " " + min;
-  }
-
-// console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"))
+function highAndLow(numbers)
+{
+  let nums = numbers.split(" ").map(Number).filter(function(v){ return v===v });
+  return Math.max(...nums) + " " + Math.min(...nums);
+}
